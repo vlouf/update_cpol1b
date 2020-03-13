8f7ed72adae66908eb8d18e81d3aad7df4ee642b
Valentin Louf <valentin.louf@bom.gov.au>
2020-03-13 12:08:46 +1100

    MODIF: pep8 formatting

--- update_cpol1b/reprocess_v2018.py
+++ update_cpol1b/reprocess_v2018.py
@@ -24,53 +24,55 @@ import xarray as xr
 
 
 def good_keys():
-    '''
+    """
     List of keys to keep in the final dataset.
-    '''
-    keep_keys = ['time',
-                 'range',
-                 'azimuth',
-                 'elevation',
-                 'radar_echo_classification',
-                 'radar_estimated_rain_rate',
-                 'velocity',
-                 'total_power',
-                 'reflectivity',
-                 'cross_correlation_ratio',
-                 'differential_reflectivity',
-                 'corrected_differential_reflectivity',
-                 'differential_phase',
-                 'corrected_differential_phase',
-                 'corrected_specific_differential_phase',
-                 'spectrum_width',
-                 'signal_to_noise_ratio',
-                 'sweep_number',
-                 'fixed_angle',
-                 'sweep_start_ray_index',
-                 'sweep_end_ray_index',
-                 'sweep_mode',
-                 'prt_mode',
-                 'prt',
-                 'nyquist_velocity',
-                 'unambiguous_range',
-                 'radar_beam_width_h',
-                 'radar_beam_width_v',
-                 'latitude',
-                 'longitude',
-                 'altitude',
-                 'time_coverage_start',
-                 'time_coverage_end',
-                 'time_reference',
-                 'volume_number',
-                 'platform_type',
-                 'instrument_type',
-                 'primary_axis']
+    """
+    keep_keys = [
+        "time",
+        "range",
+        "azimuth",
+        "elevation",
+        "radar_echo_classification",
+        "radar_estimated_rain_rate",
+        "velocity",
+        "total_power",
+        "reflectivity",
+        "cross_correlation_ratio",
+        "differential_reflectivity",
+        "corrected_differential_reflectivity",
+        "differential_phase",
+        "corrected_differential_phase",
+        "corrected_specific_differential_phase",
+        "spectrum_width",
+        "signal_to_noise_ratio",
+        "sweep_number",
+        "fixed_angle",
+        "sweep_start_ray_index",
+        "sweep_end_ray_index",
+        "sweep_mode",
+        "prt_mode",
+        "prt",
+        "nyquist_velocity",
+        "unambiguous_range",
+        "radar_beam_width_h",
+        "radar_beam_width_v",
+        "latitude",
+        "longitude",
+        "altitude",
+        "time_coverage_start",
+        "time_coverage_end",
+        "time_reference",
+        "volume_number",
+        "platform_type",
+        "instrument_type",
+        "primary_axis",
+    ]
 
     return keep_keys
 
 
 def get_metadata(radar_start_date, radar_end_date):
-    '''
+    """
     Generates metadata compatible with CF and ACDD conventions.
 
     Parameters:
@@ -84,85 +86,89 @@ def get_metadata(radar_start_date, radar_end_date):
     ========
     metadata: dict
         Metadata attributes dictionnary.
-    '''
+    """
     maxlon = 132.385
     minlon = 129.703
     maxlat = -10.941
     minlat = -13.552
-    origin_altitude = '50'
-    origin_latitude = '-12.2491'
-    origin_longitude = '131.0444'
+    origin_altitude = "50"
+    origin_latitude = "-12.2491"
+    origin_longitude = "131.0444"
     unique_id = str(uuid.uuid4())
-    fieldnames = ['radar_echo_classification',
-                  'radar_estimated_rain_rate',
-                  'velocity',
-                  'total_power',
-                  'reflectivity',
-                  'cross_correlation_ratio',
-                  'differential_reflectivity',
-                  'corrected_differential_reflectivity',
-                  'differential_phase',
-                  'corrected_differential_phase',
-                  'corrected_specific_differential_phase',
-                  'spectrum_width',
-                  'signal_to_noise_ratio']
+    fieldnames = [
+        "radar_echo_classification",
+        "radar_estimated_rain_rate",
+        "velocity",
+        "total_power",
+        "reflectivity",
+        "cross_correlation_ratio",
+        "differential_reflectivity",
+        "corrected_differential_reflectivity",
+        "differential_phase",
+        "corrected_differential_phase",
+        "corrected_specific_differential_phase",
+        "spectrum_width",
+        "signal_to_noise_ratio",
+    ]
 
-    metadata = {'Conventions': "CF-1.6, ACDD-1.3",
-                'acknowledgement': 'This work has been supported by the U.S. Department of Energy Atmospheric Systems Research Program through the grant DE-SC0014063. Data may be freely distributed.',
-                'country': 'Australia',
-                'creator_email': 'valentin.louf@bom.gov.au',
-                'creator_name': 'Valentin Louf',
-                'creator_url': 'github.com/vlouf',
-                'date_modified': datetime.datetime.now().isoformat(),
-                'field_names': ", ".join(fieldnames),
-                "geospatial_bounds": f"POLYGON(({minlon:0.6} {minlat:0.6},{minlon:0.6} {maxlat:0.6},{maxlon:0.6} {maxlat:0.6},{maxlon:0.6} {minlat:0.6},{minlon:0.6} {minlat:0.6}))",
-                'geospatial_lat_max': f'{maxlat:0.6}',
-                'geospatial_lat_min': f'{minlat:0.6}',
-                'geospatial_lat_units': "degrees_north",
-                'geospatial_lon_max': f'{maxlon:0.6}',
-                'geospatial_lon_min': f'{minlon:0.6}',
-                'geospatial_lon_units': "degrees_east",
-                'id': unique_id,
-                'institution': 'Bureau of Meteorology',
-                'instrument': 'radar',
-                'instrument_name': 'CPOL',
-                'instrument_type': 'radar',
-                'keywords': 'radar, tropics, Doppler, dual-polarization',
-                'licence': "Freely Distributed",
-                'naming_authority': 'au.org.nci',
-                'origin_altitude': origin_altitude,
-                'origin_latitude': origin_latitude,
-                'origin_longitude': origin_longitude,
-                'platform_is_mobile': 'false',
-                'processing_level': 'b1',
-                'project': "CPOL",
-                'publisher_name': "NCI",
-                'publisher_url': "nci.gov.au",
-                'references': 'doi:10.1175/JTECH-D-18-0007.1',
-                'site_name': 'Gunn Pt',
-                'source': 'radar',
-                'state': "NT",
-                'standard_name_vocabulary': 'CF Standard Name Table v71',
-                'summary': "Volumetric scan from CPOL dual-polarization Doppler radar (Darwin, Australia)",
-                'time_coverage_start': str(radar_start_date),
-                'time_coverage_end': str(radar_end_date),
-                'time_coverage_duration': "P10M",
-                'time_coverage_resolution': "PT10M",
-                'title': "radar PPI volume from CPOL",
-                'uuid': unique_id}
+    metadata = {
+        "Conventions": "CF-1.6, ACDD-1.3",
+        "acknowledgement": "This work has been supported by the U.S. Department of Energy Atmospheric Systems Research Program through the grant DE-SC0014063. Data may be freely distributed.",
+        "country": "Australia",
+        "creator_email": "valentin.louf@bom.gov.au",
+        "creator_name": "Valentin Louf",
+        "creator_url": "github.com/vlouf",
+        "date_modified": datetime.datetime.now().isoformat(),
+        "field_names": ", ".join(fieldnames),
+        "geospatial_bounds": f"POLYGON(({minlon:0.6} {minlat:0.6},{minlon:0.6} {maxlat:0.6},{maxlon:0.6} {maxlat:0.6},{maxlon:0.6} {minlat:0.6},{minlon:0.6} {minlat:0.6}))",
+        "geospatial_lat_max": f"{maxlat:0.6}",
+        "geospatial_lat_min": f"{minlat:0.6}",
+        "geospatial_lat_units": "degrees_north",
+        "geospatial_lon_max": f"{maxlon:0.6}",
+        "geospatial_lon_min": f"{minlon:0.6}",
+        "geospatial_lon_units": "degrees_east",
+        "id": unique_id,
+        "institution": "Bureau of Meteorology",
+        "instrument": "radar",
+        "instrument_name": "CPOL",
+        "instrument_type": "radar",
+        "keywords": "radar, tropics, Doppler, dual-polarization",
+        "licence": "Freely Distributed",
+        "naming_authority": "au.org.nci",
+        "origin_altitude": origin_altitude,
+        "origin_latitude": origin_latitude,
+        "origin_longitude": origin_longitude,
+        "platform_is_mobile": "false",
+        "processing_level": "b1",
+        "project": "CPOL",
+        "publisher_name": "NCI",
+        "publisher_url": "nci.gov.au",
+        "references": "doi:10.1175/JTECH-D-18-0007.1",
+        "site_name": "Gunn Pt",
+        "source": "radar",
+        "state": "NT",
+        "standard_name_vocabulary": "CF Standard Name Table v71",
+        "summary": "Volumetric scan from CPOL dual-polarization Doppler radar (Darwin, Australia)",
+        "time_coverage_start": str(radar_start_date),
+        "time_coverage_end": str(radar_end_date),
+        "time_coverage_duration": "P10M",
+        "time_coverage_resolution": "PT10M",
+        "title": "radar PPI volume from CPOL",
+        "uuid": unique_id,
+    }
 
     return metadata
 
 
 def mkdir(path):
-    '''
+    """
     Create a directory.
 
     Parameters:
     ===========
     path: str
         Path to directory
-    '''
+    """
     try:
         os.mkdir(path)
     except FileExistsError:
@@ -172,14 +178,14 @@ def mkdir(path):
 
 
 def remove(flist):
-    '''
+    """
     Remove files.
 
     Parameters:
     ===========
     flist: list
         List of files to remove
-    '''
+    """
     for f in flist:
         if f is None:
             continue
@@ -191,7 +197,7 @@ def remove(flist):
 
 
 def extract_zip(inzip, path):
-    '''
+    """
     Extract all members from the archive.
 
     Parameters:
@@ -207,8 +213,8 @@ def extract_zip(inzip, path):
         Date string for the zipfile
     namelist: list
         List of file names in the archive.
-    '''
-    dates = os.path.basename(inzip).replace('.zip', '')
+    """
+    dates = os.path.basename(inzip).replace(".zip", "")
     with zipfile.ZipFile(inzip) as zid:
         zid.extractall(path=path)
         namelist = [os.path.join(path, f) for f in zid.namelist()]
@@ -236,10 +242,12 @@ def update_dataset(radar_file, path):
     radar_start_date = dset.time[0].values
     radar_end_date = dset.time[-1].values
 
-    fname = "twp10cpolppi.b1.{}00.nc".format(pd.Timestamp(radar_start_date).strftime("%Y%m%d.%H%M"))
+    fname = "twp10cpolppi.b1.{}00.nc".format(
+        pd.Timestamp(radar_start_date).strftime("%Y%m%d.%H%M")
+    )
     outfilename = os.path.join(path, fname)
     if os.path.exists(outfilename):
-        print(f'File already exists.')
+        print(f"File already exists.")
         return None
 
     keep_keys = good_keys()
@@ -249,15 +257,21 @@ def update_dataset(radar_file, path):
             dset = dset.drop(k)
 
     metadata = get_metadata(radar_start_date, radar_end_date)
-    metadata['product_version'] = "v" + dset.attrs['product_version']
-    metadata['version'] = "v" + dset.attrs['product_version']
-    metadata['date_created'] = dset.attrs['created'],
-    metadata['history'] = "created by Valentin Louf on raijin.nci.org.au at " + dset.attrs['created'] + " using Py-ART",
+    metadata["product_version"] = "v" + dset.attrs["product_version"]
+    metadata["version"] = "v" + dset.attrs["product_version"]
+    metadata["date_created"] = (dset.attrs["created"],)
+    metadata["history"] = (
+        "created by Valentin Louf on raijin.nci.org.au at "
+        + dset.attrs["created"]
+        + " using Py-ART",
+    )
     dset.attrs = metadata
 
-    dset.to_netcdf(outfilename, encoding={k:{'zlib': True} for k in dset.variables.keys()})
+    dset.to_netcdf(
+        outfilename, encoding={k: {"zlib": True} for k in dset.variables.keys()}
+    )
     if not os.path.exists(outfilename):
-        print(f'Output file does not exist !!!.')
+        print(f"Output file does not exist !!!.")
         return None
 
     del dset
@@ -265,21 +279,23 @@ def update_dataset(radar_file, path):
 
 
 def main():
-    '''
+    """
     1/ List all zip files for a given year,
     2/ Extract one zip file (each representing one day) at a time,
     3/ Processing (updating) and removing obsolete keys,
     4/ Removing extracted files.
-    '''
-    zipdir = '/scratch/kl02/vhl548'
-    ziplist = sorted(glob.glob(f'/g/data/hj10/admin/cpol_level_1b/v2018/ppi/{YEAR}/*.zip'))
+    """
+    zipdir = "/scratch/kl02/vhl548"
+    ziplist = sorted(
+        glob.glob(f"/g/data/hj10/admin/cpol_level_1b/v2018/ppi/{YEAR}/*.zip")
+    )
     if len(ziplist) == 0:
-        print('No file found.')
+        print("No file found.")
         return None
 
     for zfile in ziplist:
         dates, namelist = extract_zip(zfile, zipdir)
-        outpath = f'/scratch/kl02/vhl548/tmpcpol/{dates}'
+        outpath = f"/scratch/kl02/vhl548/tmpcpol/{dates}"
         mkdir(outpath)
         bag = db.from_sequence([(n, outpath) for n in namelist]).starmap(update_dataset)
         rslt = bag.compute()
@@ -291,12 +307,9 @@ def main():
 if __name__ == "__main__":
     parser_description = "Update and re-encode version v2018 of 1b CPOL product."
     parser = argparse.ArgumentParser(description=parser_description)
-    parser.add_argument('-y',
-                        '--year',
-                        dest='year',
-                        type=int,
-                        help='Year to process.',
-                        required=True)
+    parser.add_argument(
+        "-y", "--year", dest="year", type=int, help="Year to process.", required=True
+    )
 
     args = parser.parse_args()
     YEAR = args.year
--- update_cpol1b/update_cpol1b.py
+++ update_cpol1b/update_cpol1b.py
@@ -30,51 +30,57 @@ def chunks(l, n):
     From http://stackoverflow.com/a/312464
     """
     for i in range(0, len(l), n):
-        yield l[i:i + n]
+        yield l[i : i + n]
 
 
 def get_metadata():
     today = datetime.datetime.utcnow()
-    maxlon = '132.385'
-    minlon = '129.703'
-    maxlat = '-10.941'
-    minlat = '-13.552'
-    origin_altitude = '50'
-    origin_latitude = '-12.2488'
-    origin_longitude = '131.0444'
+    maxlon = "132.385"
+    minlon = "129.703"
+    maxlat = "-10.941"
+    minlat = "-13.552"
+    origin_altitude = "50"
+    origin_latitude = "-12.2488"
+    origin_longitude = "131.0444"
 
     metadata = dict()
-    metadata['Conventions'] = "CF/Radial instrument_parameters"
-    metadata['acknowledgement'] = 'This work has been supported by the U.S. Department of Energy Atmospheric Systems Research Program through the grant DE-SC0014063. Data may be freely distributed.'
-    metadata['country'] = 'Australia'
-    metadata['creator_email'] = 'valentin.louf@monash.edu'
-    metadata['creator_name'] = 'Valentin Louf'
-    metadata['geospatial_bounds'] = f"({minlon}, {maxlon}, {minlat}, {maxlat})"
-    metadata['geospatial_lat_max'] = maxlat
-    metadata['geospatial_lat_min'] = minlat
-    metadata['geospatial_lat_units'] = "degrees_north"
-    metadata['geospatial_lon_max'] = maxlon
-    metadata['geospatial_lon_min'] = minlon
-    metadata['geospatial_lon_units'] = "degrees_east"
-    metadata['history'] = "created by Valentin Louf on raijin.nci.org.au at " + today.isoformat() + " using Py-ART"
-    metadata['institution'] = 'Monash University and Australian Bureau of Meteorology'
-    metadata['instrument_name'] = 'CPOL'
-    metadata['instrument_type'] = 'radar'
-    metadata['naming_authority'] = 'au.org.nci'
-    metadata['origin_altitude'] = origin_altitude
-    metadata['origin_latitude'] = origin_latitude
-    metadata['origin_longitude'] = origin_longitude
-    metadata['platform_is_mobile'] = 'false'
-    metadata['processing_level'] = 'b1'
-    metadata['publisher_name'] = "NCI"
-    metadata['publisher_url'] = "nci.gov.au"
-    metadata['references'] = 'cf. doi:10.1175/JTECH-D-18-0007.1'
-    metadata['site_name'] = 'Gunn_Pt'
-    metadata['source'] = 'rapic'
-    metadata['state'] = "NT"
-    metadata['title'] = "radar PPI volume from CPOL"
-    metadata['uuid'] = str(uuid.uuid4())
-    metadata['version'] = "1.3"
+    metadata["Conventions"] = "CF/Radial instrument_parameters"
+    metadata[
+        "acknowledgement"
+    ] = "This work has been supported by the U.S. Department of Energy Atmospheric Systems Research Program through the grant DE-SC0014063. Data may be freely distributed."
+    metadata["country"] = "Australia"
+    metadata["creator_email"] = "valentin.louf@monash.edu"
+    metadata["creator_name"] = "Valentin Louf"
+    metadata["geospatial_bounds"] = f"({minlon}, {maxlon}, {minlat}, {maxlat})"
+    metadata["geospatial_lat_max"] = maxlat
+    metadata["geospatial_lat_min"] = minlat
+    metadata["geospatial_lat_units"] = "degrees_north"
+    metadata["geospatial_lon_max"] = maxlon
+    metadata["geospatial_lon_min"] = minlon
+    metadata["geospatial_lon_units"] = "degrees_east"
+    metadata["history"] = (
+        "created by Valentin Louf on raijin.nci.org.au at "
+        + today.isoformat()
+        + " using Py-ART"
+    )
+    metadata["institution"] = "Monash University and Australian Bureau of Meteorology"
+    metadata["instrument_name"] = "CPOL"
+    metadata["instrument_type"] = "radar"
+    metadata["naming_authority"] = "au.org.nci"
+    metadata["origin_altitude"] = origin_altitude
+    metadata["origin_latitude"] = origin_latitude
+    metadata["origin_longitude"] = origin_longitude
+    metadata["platform_is_mobile"] = "false"
+    metadata["processing_level"] = "b1"
+    metadata["publisher_name"] = "NCI"
+    metadata["publisher_url"] = "nci.gov.au"
+    metadata["references"] = "cf. doi:10.1175/JTECH-D-18-0007.1"
+    metadata["site_name"] = "Gunn_Pt"
+    metadata["source"] = "rapic"
+    metadata["state"] = "NT"
+    metadata["title"] = "radar PPI volume from CPOL"
+    metadata["uuid"] = str(uuid.uuid4())
+    metadata["version"] = "1.3"
 
     return metadata
 
@@ -90,9 +96,11 @@ def update_data(infile):
     """
     radar = pyart.io.read(infile)
 
-    radar_start_date = netCDF4.num2date(radar.time['data'][0], radar.time['units'])
+    radar_start_date = netCDF4.num2date(radar.time["data"][0], radar.time["units"])
     daystr = radar_start_date.strftime("%Y%m%d")
-    filename = "twp10cpolppi.b1.{}00.nc".format(radar_start_date.strftime("%Y%m%d.%H%M"))
+    filename = "twp10cpolppi.b1.{}00.nc".format(
+        radar_start_date.strftime("%Y%m%d.%H%M")
+    )
     outdir = os.path.join(OUTPATH, str(radar_start_date.year))
     try:
         os.mkdir(outdir)
@@ -110,7 +118,9 @@ def update_data(infile):
     # Get original level 1a file for copying intstrument parameters
     datestr = radar_start_date.strftime("%Y%m%d.%H%M")
     yrstr = radar_start_date.strftime("%Y")
-    fone = "/g/data/hj10/cpol_level_1a/v2019/ppi/{}/{}/twp10cpolppi.a1.{}00.nc".format(yrstr, daystr, datestr)
+    fone = "/g/data/hj10/cpol_level_1a/v2019/ppi/{}/{}/twp10cpolppi.a1.{}00.nc".format(
+        yrstr, daystr, datestr
+    )
     if not os.path.isfile(fone):
         print(f"{fone} is missing.")
         fone = "/g/data/hj10/cpol_level_1a/v2019/ppi/2017/20170304/twp10cpolppi.a1.20170304.000000.nc"
@@ -121,10 +131,12 @@ def update_data(infile):
     radar.radar_calibration = None
 
     # Drop Obsolete keys
-    keys_drop = ['temperature',
-        'specific_attenuation_reflectivity',
-        'specific_attenuation_differential_reflectivity',
-        'velocity_texture',]
+    keys_drop = [
+        "temperature",
+        "specific_attenuation_reflectivity",
+        "specific_attenuation_differential_reflectivity",
+        "velocity_texture",
+    ]
 
     klist = list(radar.fields.keys())
     for key in klist:
@@ -133,68 +145,78 @@ def update_data(infile):
 
     # Rename the reflectivity
     try:
-        radar.add_field('corrected_reflectivity', radar.fields.pop('reflectivity'))
-        radar.fields['corrected_reflectivity']['data'] = radar.fields['corrected_reflectivity']['data'].filled(np.NaN)
-        radar.fields['corrected_reflectivity']['data'] = np.ma.masked_invalid(radar.fields['corrected_reflectivity']['data'])
-        np.ma.set_fill_value(radar.fields['corrected_reflectivity']['data'], np.NaN)
+        radar.add_field("corrected_reflectivity", radar.fields.pop("reflectivity"))
+        radar.fields["corrected_reflectivity"]["data"] = radar.fields[
+            "corrected_reflectivity"
+        ]["data"].filled(np.NaN)
+        radar.fields["corrected_reflectivity"]["data"] = np.ma.masked_invalid(
+            radar.fields["corrected_reflectivity"]["data"]
+        )
+        np.ma.set_fill_value(radar.fields["corrected_reflectivity"]["data"], np.NaN)
     except Exception:
         traceback.print_exc()
         pass
 
     # Rename the 2 Velocity fields
     try:
-        radar.fields['velocity'] = radar.fields.pop('raw_velocity')
-        radar.add_field('corrected_velocity', radar.fields.pop('region_dealias_velocity'))
+        radar.fields["velocity"] = radar.fields.pop("raw_velocity")
+        radar.add_field(
+            "corrected_velocity", radar.fields.pop("region_dealias_velocity")
+        )
     except Exception:
         pass
 
     # Make sure echo class is an integer array and fix its fillvalue.
     try:
-        radar.fields['radar_echo_classification']['data'] = radar.fields['radar_echo_classification']['data'].astype(np.int32)
-        np.ma.set_fill_value(radar.fields['radar_echo_classification']['data'], -9999)
-        radar.fields['radar_echo_classification']['_FillValue'] = -9999
+        radar.fields["radar_echo_classification"]["data"] = radar.fields[
+            "radar_echo_classification"
+        ]["data"].astype(np.int32)
+        np.ma.set_fill_value(radar.fields["radar_echo_classification"]["data"], -9999)
+        radar.fields["radar_echo_classification"]["_FillValue"] = -9999
     except Exception:
         pass
 
-    radar.fields['radar_estimated_rain_rate']['_Least_significant_digit'] = 2
+    radar.fields["radar_estimated_rain_rate"]["_Least_significant_digit"] = 2
 
     try:
-        radar.fields['NW'].pop('standard_name')
-        np.ma.set_fill_value(radar.fields['NW']['data'], np.NaN)
-        radar.fields['NW']['_FillValue'] = np.NaN
-        radar.fields['NW']['_Least_significant_digit'] = 2
-        radar.fields['NW']['data'] = np.ma.masked_invalid(radar.fields['NW']['data'])
+        radar.fields["NW"].pop("standard_name")
+        np.ma.set_fill_value(radar.fields["NW"]["data"], np.NaN)
+        radar.fields["NW"]["_FillValue"] = np.NaN
+        radar.fields["NW"]["_Least_significant_digit"] = 2
+        radar.fields["NW"]["data"] = np.ma.masked_invalid(radar.fields["NW"]["data"])
     except Exception:
         pass
 
-    klist_pr = [('D0', 2, np.NaN),
-                ('velocity', 2, np.NaN),
-                ('total_power', 2, np.NaN),
-                ('corrected_reflectivity', 2, np.NaN),
-                ('cross_correlation_ratio', 4, np.NaN),
-                ('corrected_differential_reflectivity', 4, np.NaN),
-                ('corrected_differential_phase', 4, np.NaN),
-                ('corrected_specific_differential_phase', 4, np.NaN),
-                ('differential_reflectivity', 4, np.NaN),
-                ('differential_phase', 4, np.NaN),
-                ('spectrum_width', 4, np.NaN),
-                ('signal_to_noise_ratio', 2, np.NaN),
-                ('corrected_velocity', 2, np.NaN)]
+    klist_pr = [
+        ("D0", 2, np.NaN),
+        ("velocity", 2, np.NaN),
+        ("total_power", 2, np.NaN),
+        ("corrected_reflectivity", 2, np.NaN),
+        ("cross_correlation_ratio", 4, np.NaN),
+        ("corrected_differential_reflectivity", 4, np.NaN),
+        ("corrected_differential_phase", 4, np.NaN),
+        ("corrected_specific_differential_phase", 4, np.NaN),
+        ("differential_reflectivity", 4, np.NaN),
+        ("differential_phase", 4, np.NaN),
+        ("spectrum_width", 4, np.NaN),
+        ("signal_to_noise_ratio", 2, np.NaN),
+        ("corrected_velocity", 2, np.NaN),
+    ]
 
     for key, least_digit, fvalue in klist_pr:
         if key not in klist:
             continue
         try:
-            np.ma.set_fill_value(radar.fields[key]['data'], fvalue)
-            radar.fields[key]['data'] = radar.fields[key]['data'].astype(np.float32)
-            radar.fields[key]['_Least_significant_digit'] = least_digit
-            radar.fields[key]['_FillValue'] = fvalue
+            np.ma.set_fill_value(radar.fields[key]["data"], fvalue)
+            radar.fields[key]["data"] = radar.fields[key]["data"].astype(np.float32)
+            radar.fields[key]["_Least_significant_digit"] = least_digit
+            radar.fields[key]["_FillValue"] = fvalue
         except Exception:
             traceback.print_exc()
             continue
 
     # Remove wrongfull attributes.
-    bad_attr = ['grid_mapping', 'coordinates', 'sampling_ratio']
+    bad_attr = ["grid_mapping", "coordinates", "sampling_ratio"]
     for k in radar.fields.keys():
         for badk in bad_attr:
             try:
@@ -228,7 +250,7 @@ def process_one_file(input_file):
 
 
 def main():
-    indir = os.path.join(INPATH, str(YEAR), '**', '*.nc')
+    indir = os.path.join(INPATH, str(YEAR), "**", "*.nc")
     flist = sorted(glob.glob(indir))
 
     print(f"Found {len(flist)} files.")
@@ -260,23 +282,29 @@ if __name__ == "__main__":
 
     parser_description = "Update and re-encode previous version of 1b CPOL product."
     parser = argparse.ArgumentParser(description=parser_description)
-    parser.add_argument('-y',
-        '--year',
-        dest='year',
+    parser.add_argument(
+        "-y",
+        "--year",
+        dest="year",
         default=2017,
         type=int,
-        help='Year to process. Uses multiprocessing to do so. Superseeded by the --file argument.')
-    parser.add_argument('-f',
-        '--file',
-        dest='file',
+        help="Year to process. Uses multiprocessing to do so. Superseeded by the --file argument.",
+    )
+    parser.add_argument(
+        "-f",
+        "--file",
+        dest="file",
         type=str,
-        help='Input file. This option superseeds the --year argument.')
-    parser.add_argument('-o',
-        '--output',
-        dest='output',
+        help="Input file. This option superseeds the --year argument.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="output",
         default="/g/data/hj10/cpol_level_1b/v2018/ppi",
         type=str,
-        help='Output directory')
+        help="Output directory",
+    )
 
     args = parser.parse_args()
     YEAR = args.year
